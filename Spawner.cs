d7940e505c1d952be55831bed1a0338b6d877118
David Mannes <git@davidmannes.de>
2023-03-08 13:10:57 +0100

    Add enemy spawner

---
+++ Spawner.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+public partial class Spawner : Node
+{
+    [Export]
+    public PackedScene EnemyScene { get; set; }
+
+    public void SpawnEnemy()
+    {
+        SpawnEnemy(Vector2.Zero);
+    }
+
+    public void SpawnEnemy(Vector2 position)
+    {
+        var enemy = EnemyScene.Instantiate<Node2D>();
+        enemy.Position = position;
+        GetNode("../").AddChild(enemy);
+    }
+}
