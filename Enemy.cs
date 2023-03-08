5ae37d0443cd0670b6b8403c4f1fcf0aced06dc5
David Mannes <git@davidmannes.de>
2023-03-08 12:52:11 +0100

    Add movement to enemy

---
+++ Enemy.cs
@@ -0,0 +1,16 @@
+using Godot;
+using Microsoft.VisualBasic;
+
+public partial class Enemy : CharacterBody2D
+{
+    [Export]
+    public float Speed { get; set; }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var player = GetNode<Node2D>("../Player");
+        var direction = GlobalPosition.DirectionTo(player.GlobalPosition);
+        Velocity = direction * Speed;
+        MoveAndSlide();
+	}
+}
