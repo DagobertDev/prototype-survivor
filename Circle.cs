824d355242e531936b7cc1b2a4dbde446ef8640e
David Mannes <git@davidmannes.de>
2023-03-28 20:40:22 +0200

    Add pushback to circle

--- Circle.cs
+++ Circle.cs
@@ -9,14 +9,30 @@ public partial class Circle : Node2D
 	
 	[Export]
 	public int Radius { get; set; }
+	
+	[Export]
+	public int PushBackDistance { get; set; }
 
 	public override void _Ready()
 	{
-		GetChild<Node2D>(0).Position = Vector2.Right * Radius;
+		var area = GetChild<Area2D>(0);
+		area.Position = Vector2.Right * Radius;
+		area.BodyEntered += OnBodyEntered;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		RotationDegrees += RotationSpeed * (float)delta;
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is not Enemy enemy)
+		{
+			return;
+		}
+
+		var pushBack = PushBackDistance * GlobalPosition.DirectionTo(enemy.GlobalPosition);
+		enemy.MoveAndCollide(pushBack);
 	}
 }
