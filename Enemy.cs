b91b781df5aa292e7fff6b698c43266df837dba9
David Mannes <git@davidmannes.de>
2023-03-28 20:47:20 +0200

    Randomize enemy movement

--- Enemy.cs
+++ Enemy.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.ObjectModel;
 using Godot;
 
 namespace PrototypeSurvivor;
@@ -7,11 +9,27 @@ public partial class Enemy : CharacterBody2D
 	[Export]
 	public float Speed { get; set; }
 
+	private Node2D _player;
+	
+	private Vector2 destination;
+
+	public override void _Ready()
+	{
+		_player = GetNode<Node2D>("../Player");
+		UpdateDestination();
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
-		var player = GetNode<Node2D>("../Player");
-		var direction = GlobalPosition.DirectionTo(player.GlobalPosition);
+		var direction = GlobalPosition.DirectionTo(destination);
 		Velocity = direction * Speed;
 		MoveAndSlide();
+	}
+
+	public void UpdateDestination()
+	{
+		destination = _player.GlobalPosition;
+		destination.X += Random.Shared.Next(-100, 100);
+		destination.Y += Random.Shared.Next(-100, 100);
 	}
 }
